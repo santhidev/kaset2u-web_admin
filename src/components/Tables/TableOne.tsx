de4b52ba9917f8529f3035125f9193396c748603
Athit <144748590+AthitWanchai@users.noreply.github.com>
2025-01-21 20:17:51 +0700

    แก้ไข ข้อมูลตาราง

--- src/components/Tables/TableOne.tsx
+++ src/components/Tables/TableOne.tsx
@@ -48,11 +48,11 @@ const TableOne = () => {
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-        Top Channels
+        หน้ารายการสินค้า
       </h4>
 
       <div className="flex flex-col">
-        <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
+        <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-6">
           <div className="p-2.5 xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
               Source
@@ -78,15 +78,19 @@ const TableOne = () => {
               Conversion
             </h5>
           </div>
+          <div className="hidden p-2.5 text-center sm:block xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Conversion
+            </h5>
+          </div>
         </div>
 
         {brandData.map((brand, key) => (
           <div
-            className={`grid grid-cols-3 sm:grid-cols-5 ${
-              key === brandData.length - 1
-                ? ""
-                : "border-b border-stroke dark:border-strokedark"
-            }`}
+            className={`grid grid-cols-3 sm:grid-cols-5 ${key === brandData.length - 1
+              ? ""
+              : "border-b border-stroke dark:border-strokedark"
+              }`}
             key={key}
           >
             <div className="flex items-center gap-3 p-2.5 xl:p-5">
